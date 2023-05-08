c6435bb31deb089856c9ac8660eb87d5be48f413
guiguivida183882 <guikerme.1707@gmail.com>
2023-05-07 21:07:11 -0300

    estrutura do header

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Responsive Menu</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+	<nav>
+		<input type="checkbox" id="check">
+		<label for="check" class="checkbtn">
+			<i class="fas fa-bars"></i>
+		</label>
+		<img src="img/logopequenaweb.png" alt="" class="logo">
+		<ul>
+			<li><a class="active" href="#">Início</a></li>
+			<li><a href="#">Sobre</a></li>
+			<li><a href="#">Serviços</a></li>
+			<li><a href="#">Contato</a></li>
+		</ul>
+	</nav>
+</body>
+</html>
